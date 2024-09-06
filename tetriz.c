eb33746e0a95ea17c40ff01401614b77c074d9fb
Izmar Verhage <izmaelverhage@gmail.com>
2024-09-06 12:14:48 +0200

    Add test animation

--- tetriz.c
+++ tetriz.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 
 const uint8_t WIDTH = 80;
 const uint8_t HEIGHT = 40;
@@ -8,46 +9,65 @@ char canvas[HEIGHT][WIDTH];
 
 void clear_canvas()
 {
-	for (uint8_t y = 0; y < HEIGHT; y++)
-	{
-		for (uint8_t x = 0; x < WIDTH; x++)
-		{
-			canvas[y][x] = ' ';
-		}
-	}
+    for (uint8_t y = 0; y < HEIGHT; y++)
+    {
+        for (uint8_t x = 0; x < WIDTH; x++)
+        {
+            canvas[y][x] = ' ';
+        }
+    }
 }
 
 void draw_border()
 {
-	for (uint8_t y = 0; y < HEIGHT; y++)
-	{
-		for (uint8_t x = 0; x < WIDTH; x++)
-		{
-			if (y > 0 && y < (HEIGHT - 1) && x != 0 && x != WIDTH - 1)
-			{
-			    continue;
-			}
-			
-			canvas[y][x] = '#';
-		}
-	}
+    for (uint8_t y = 0; y < HEIGHT; y++)
+    {
+        for (uint8_t x = 0; x < WIDTH; x++)
+        {
+            if (y > 0 && y < (HEIGHT - 1) && x != 0 && x != WIDTH - 1)
+            {
+                continue;
+            }
+
+            canvas[y][x] = '#';
+        }
+    }
 }
 
 void draw_canvas_to_screen()
 {
-	for (uint8_t y = 0; y < HEIGHT; y++)
-	{
-		for (uint8_t x = 0; x < WIDTH; x++)
-		{
-		    printf("%c", canvas[y][x]);
-		}
-		printf("\n");
-	}
+    for (uint8_t y = 0; y < HEIGHT; y++)
+    {
+        for (uint8_t x = 0; x < WIDTH; x++)
+        {
+            printf("%c", canvas[y][x]);
+        }
+        printf("\n");
+    }
+}
+
+void clear_screen()
+{
+    printf("\033[H\033[J");
+}
+
+uint8_t foo_x = 1;
+
+void draw_foo()
+{
+    canvas[1][foo_x] = 'O';
+    foo_x++;
 }
 
 int main()
 {
-	clear_canvas();
-	draw_border();
-	draw_canvas_to_screen();
+    for (uint8_t i = 0; i < 10; i++)
+    {
+        clear_screen();
+        clear_canvas();
+        draw_border();
+        draw_foo();
+        draw_canvas_to_screen();
+        usleep(50000);
+    }
 }
