d488324a85d442df43adc0d04f0402764dc0548a
Izmar Verhage <izmaelverhage@gmail.com>
2024-09-21 12:15:02 +0200

    Add 180 degrees rotation test for all tetrominos

--- src/test/testriz.c
+++ src/test/testriz.c
@@ -65,9 +65,43 @@ void test_90_degrees_rotation()
     check_tetrominos_equal(expected_z, rotated_z);
 }
 
+void test_180_degrees_rotation()
+{
+    Tetromino* unrotated_i = &TETROMINOS[TETROMINO_I];
+    Tetromino* rotated_i = rotate(unrotated_i, ROTATED_180_DEGREES);
+    Tetromino* expected_i = &TETROMINOS_ROTATED_180[0];
+    check_tetrominos_equal(expected_i, rotated_i);
+
+    Tetromino* unrotated_t = &TETROMINOS[TETROMINO_T];
+    Tetromino* rotated_t = rotate(unrotated_t, ROTATED_180_DEGREES);
+    Tetromino* expected_t = &TETROMINOS_ROTATED_180[1];
+    check_tetrominos_equal(expected_t, rotated_t);
+
+    Tetromino* unrotated_l = &TETROMINOS[TETROMINO_L];
+    Tetromino* rotated_l = rotate(unrotated_l, ROTATED_180_DEGREES);
+    Tetromino* expected_l = &TETROMINOS_ROTATED_180[2];
+    check_tetrominos_equal(expected_l, rotated_l);
+
+    Tetromino* unrotated_j = &TETROMINOS[TETROMINO_J];
+    Tetromino* rotated_j = rotate(unrotated_j, ROTATED_180_DEGREES);
+    Tetromino* expected_j = &TETROMINOS_ROTATED_180[3];
+    check_tetrominos_equal(expected_j, rotated_j);
+
+    Tetromino* unrotated_s = &TETROMINOS[TETROMINO_S];
+    Tetromino* rotated_s = rotate(unrotated_s, ROTATED_180_DEGREES);
+    Tetromino* expected_s = &TETROMINOS_ROTATED_180[4];
+    check_tetrominos_equal(expected_s, rotated_s);
+
+    Tetromino* unrotated_z = &TETROMINOS[TETROMINO_Z];
+    Tetromino* rotated_z = rotate(unrotated_z, ROTATED_180_DEGREES);
+    Tetromino* expected_z = &TETROMINOS_ROTATED_180[5];
+    check_tetrominos_equal(expected_z, rotated_z);
+}
+
 TEST_LIST = {
     { "no_rotation", test_no_rotation },
     { "90_degrees_rotation", test_90_degrees_rotation },
+    { "180_degrees_rotation", test_180_degrees_rotation },
     { NULL, NULL }
 };
 
